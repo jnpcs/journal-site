1a6596b330d19c221746fe700b3ccc6424e15065
BuildTools <belov@bsu.by>
2016-12-16 21:23:28 +0300

    кажись пофиксил индексы в форме

--- main.js
+++ main.js
@@ -2,10 +2,17 @@
 
 authorsNumber = 1;
 
+// extend standart string class
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+
 function addAuthor() {
 	var el = document.getElementById("toClone")
 	authorFormChunk = el.innerHTML;
-	authorFormChunk = authorFormChunk.replace("authors[0]","authors["+authorsNumber+"]");
+	authorFormChunk = authorFormChunk.replaceAll("authors[0]","authors["+authorsNumber+"]");
 	var newDiv = document.createElement("doc");
 	newDiv.innerHTML = authorFormChunk;
 	el.parentNode.insertBefore(newDiv,document.getElementById("plusButton"));
